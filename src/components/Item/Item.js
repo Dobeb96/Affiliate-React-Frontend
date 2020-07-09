5037db0abd654cba15e5e4e02d70fd6ad3b04bdd
Maciej Dobosz <dobosz.ma@interia.pl>
2020-07-09 20:25:49 +0200

    Remove vendor url button

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Item.scss"
 import MdHeartOutline from 'react-ionicons/lib/MdHeartOutline'
-import MdTrendingUp from 'react-ionicons/lib/MdTrendingUp'
 
 class Item extends React.Component {
     render() {
@@ -14,12 +13,6 @@ class Item extends React.Component {
             <div className="card-content-hover">
                 <div className="btn-primary btn-round-small btn-hover-darken flex-middle item-favourite" to="/">
                     <MdHeartOutline color="white" fontSize="1.5em" />
-                </div>
-                <div className="btn-primary-small btn-hover-darken item-vendor-url" to="/">
-                    <div className="item-favourite-icon">
-                        <MdTrendingUp color="white" fontSize="1.5em" />
-                    </div>
-                    <div>vendor.url</div>
                 </div>
                 <div className="item-header">
                     <h3 className="item-name">Item name</h3>
