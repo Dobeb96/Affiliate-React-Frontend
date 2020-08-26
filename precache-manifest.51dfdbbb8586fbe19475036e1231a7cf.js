aa278008bf5fe83dcb1a45044da9d944b0a8161e
Maciej Dobosz <dobosz.ma@interia.pl>
2020-08-26 20:05:56 +0200

    Updates

---
+++ precache-manifest.51dfdbbb8586fbe19475036e1231a7cf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8f03afb1b0fd9e2851547ad88fcf17a",
+    "url": "/affiliate-react-frontend/index.html"
+  },
+  {
+    "revision": "5195ec60f36d4f9cda09",
+    "url": "/affiliate-react-frontend/static/css/main.279788f6.chunk.css"
+  },
+  {
+    "revision": "8da23d22f4e56fed8756",
+    "url": "/affiliate-react-frontend/static/js/2.db82d8fd.chunk.js"
+  },
+  {
+    "revision": "56314afb9447acda61c0bb273cfa2653",
+    "url": "/affiliate-react-frontend/static/js/2.db82d8fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5195ec60f36d4f9cda09",
+    "url": "/affiliate-react-frontend/static/js/main.adec27bc.chunk.js"
+  },
+  {
+    "revision": "17b5e247a5577d737737",
+    "url": "/affiliate-react-frontend/static/js/runtime-main.7796d27f.js"
+  },
+  {
+    "revision": "10c6818802ed96f9d6976633ae148c37",
+    "url": "/affiliate-react-frontend/static/media/Anton-Regular.10c68188.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/affiliate-react-frontend/static/media/Roboto-Regular.11eabca2.ttf"
+  }
+]);
